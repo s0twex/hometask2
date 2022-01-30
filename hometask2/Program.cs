2ee0ee011c5a1f47c6f9912cdc39641be423aac5
hohmu <hohmu@DESKTOP-IK9JCN6>
2022-01-30 22:17:31 +0300

    Добавьте файлы проекта.

---
+++ hometask2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homwtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            double c = double.Parse(Console.ReadLine());
+            if (a > b) a = b;
+            if (a > c) a = c;
+            Console.WriteLine("Min = {0}", a);
+            Console.WriteLine("Нажмите ENTER");
+            Console.ReadKey();
+
+
+            Console.WriteLine("введите целое положительное число менее 2000000000");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (n != 0)
+            {
+                count++;
+                n = n / 10;
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+
+        }
+    }
+}
